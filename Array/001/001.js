12b2aa3d62eff9b71f12d6f4df9f87de88f8eb47
ttop5 <ttop5@qq.com>
2016-09-14 17:35:41 +0800

    fix(001.js): 代码简化

--- Array/001/001.js
+++ Array/001/001.js
@@ -4,12 +4,10 @@
  * @return {number[]}
  */
 function twoSum(nums, target) {
-  const narray = [];
   for (let i=0; i<nums.length; i++) {
     for (let j=i + 1; j<nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        narray.push(i) && narray.push(j);
-        return narray;
+        return [i, j];
       }
     }
   }
