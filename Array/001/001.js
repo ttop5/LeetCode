339443e7dcb20a7a2237847b06f358a41e535cf9
ttop5 <ttop5@qq.com>
2016-09-14 17:46:19 +0800

    docs(001.js)： 添加思路说明

--- Array/001/001.js
+++ Array/001/001.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
+ * 遍历数组元素分别与之后面的元素相加，满足条件就返回索引
  */
 function twoSum(nums, target) {
   for (let i=0; i<nums.length; i++) {
