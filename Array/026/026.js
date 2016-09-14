b5cfa69a9e7a344ef27d590352e45dd15985604f
ttop5 <ttop5@qq.com>
2016-09-14 19:09:17 +0800

    feat(026): 26. Remove Duplicates from Sorted Array

---
+++ Array/026/026.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 在一个数组中操作，不使用额外的新空间
+ */
+function removeDuplicates(nums) {
+  let len = 1; // 最少有一个元素不重复
+  for (let i=1; i<nums.length; i++) { // 从第2个元素开始跟前面一个比较
+    if (nums[i] !== nums[i-1]) {
+      nums[len++] = nums[i]; // 不等于前一个元素的话长度加1，并将新元素加到前len个
+    }
+  }
+  return len;
+}
---
+++ Array/026/README.md
@@ -0,0 +1,5 @@
+# 26. Remove Duplicates from Sorted Array
+
+## 题目大意：
+
+> 将数组中的重复的元素删除，并返回新数组的长度。
