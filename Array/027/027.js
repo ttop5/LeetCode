da572e7b59d146b2cba6f80dd4d60553812cea5b
何艳红 <heyh2@knownsec.com>
2016-09-27 23:58:49 +0800

    feat(027): Remove Element

---
+++ Array/027/027.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ * 使用类似于指针的思路
+ */
+function removeElement (nums, val) {
+  let j = 0;
+  for (let i=0; i<nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j++] = nums[i];
+    }
+  }
+  return j;
+}
